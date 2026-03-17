b20357cab8545d7e2a303b9153a9690e3d78fc43
d3solta1on <susik2017ruslan@gmail.com>
2026-03-17 04:22:58 +0400

    vrode norm

---
+++ Converters/PercentageConverter.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace MusicPlayer_by_d3solat1on.Converters
+{
+    public class PercentageConverter : MarkupExtension, IValueConverter
+    {
+        private static PercentageConverter? _instance;
+        
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _instance ??= new PercentageConverter();
+        }
+        
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double doubleValue && doubleValue >= 0)
+            {
+                // Возвращаем значение в процентах от максимального (100)
+                return doubleValue;
+            }
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
